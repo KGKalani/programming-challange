a4ad110032c596f5f271f949a5dc629e8dee76e6
dilshankanchana <dilshankanchana987@gmail.com>
2014-10-31 15:57:32 +0530

    commit

---
+++ tac-tic-teo/src/singlePlayer/Buttons.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package singlePlayer;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author gayathri
+ */
+public class Buttons {
+    boolean status;
+    static int chance;
+    int value;
+    JButton btn;
+    
+    public Buttons(){
+        
+        status = false;
+        chance = 0;
+        value = 0;
+    }
+}
